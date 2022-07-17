c54da6fa66f7410a32c5911b846ac4def4653c2f
Cornelius Venti <corneliusventi@gmail.com>
2022-07-17 20:48:29 +0700

    Learning React Testing Library

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import validator from "validator";
+
+import "./App.css";
 
 function App() {
+  const [signupInput, setSignupInput] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    setSignupInput({
+      ...signupInput,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (!validator.isEmail(signupInput.email)) {
+      return setError("The email you input is invalid.");
+    } else if (!validator.isLength(signupInput.password, { min: 5 })) {
+      return setError(
+        "The password you entered should contain 5 or more characters."
+      );
+    } else if (
+      !validator.equals(signupInput.password, signupInput.confirmPassword)
+    ) {
+      return setError("The passwords don't match. Try again.");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-5">
+      <form>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="form-control"
+            value={signupInput.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="form-control"
+            value={signupInput.password}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="confirmPassword" className="form-label">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            className="form-control"
+            value={signupInput.confirmPassword}
+            onChange={handleChange}
+          />
+        </div>
+        {error && <p className="text-danger">{error}</p>}
+        <button type="submit" className="btn btn-primary" onClick={handleClick}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,134 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+const typeIntoForm = ({ email, password, confirmPassword }) => {
+  const emailInputElement = screen.getByRole("textbox", { name: /email/i });
+  const passwordInputElement = screen.getByLabelText("Password");
+  const confirmPasswordInputElement =
+    screen.getByLabelText(/confirm password/i);
+
+  if (email) {
+    userEvent.type(emailInputElement, email);
+  }
+  if (password) {
+    userEvent.type(passwordInputElement, password);
+  }
+  if (confirmPassword) {
+    userEvent.type(confirmPasswordInputElement, confirmPassword);
+  }
+
+  return {
+    emailInputElement,
+    passwordInputElement,
+    confirmPasswordInputElement,
+  };
+};
+
+const clickOnSubmitButton = () => {
+  const submitBtnElement = screen.getByRole("button", {
+    name: /submit/i,
+  });
+
+  userEvent.click(submitBtnElement);
+};
+
+describe("App", () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  test("inputs should be initially empty", () => {
+    expect(screen.getByRole("textbox").value).toBe("");
+    expect(screen.getByLabelText("Password").value).toBe("");
+    expect(screen.getByLabelText(/confirm password/i).value).toBe("");
+  });
+
+  test("should be able to type an email", () => {
+    const { emailInputElement } = typeIntoForm({ email: "selena@gmail.com" });
+    expect(emailInputElement.value).toBe("selena@gmail.com");
+  });
+
+  test("should be able to type an password", () => {
+    const { passwordInputElement } = typeIntoForm({ password: "12345678" });
+    expect(passwordInputElement.value).toBe("12345678");
+  });
+
+  test("should be able to type an confirm password", () => {
+    const { confirmPasswordInputElement } = typeIntoForm({
+      confirmPassword: "12345678",
+    });
+    expect(confirmPasswordInputElement.value).toBe("12345678");
+  });
+
+  describe("Error Handling", () => {
+    test("should show email error message on invalid email", () => {
+      expect(
+        screen.queryByText(/the email you input is invalid/i)
+      ).not.toBeInTheDocument();
+
+      typeIntoForm({ email: "selenagmail.com" });
+      clickOnSubmitButton();
+
+      expect(
+        screen.queryByText(/the email you input is invalid/i)
+      ).toBeInTheDocument();
+    });
+
+    test("should show password error if password is less than 5 charaters", () => {
+      expect(
+        screen.queryByText(
+          /the password you entered should contain 5 or more characters/i
+        )
+      ).not.toBeInTheDocument();
+
+      typeIntoForm({ email: "selena@gmail.com", password: "123" });
+      clickOnSubmitButton();
+
+      expect(
+        screen.queryByText(
+          /the password you entered should contain 5 or more characters/i
+        )
+      ).toBeInTheDocument();
+    });
+
+    test("should show confirm password error if passwords don't match", () => {
+      expect(
+        screen.queryByText(/the passwords don't match. try again/i)
+      ).not.toBeInTheDocument();
+
+      typeIntoForm({
+        email: "selena@gmail.com",
+        password: "12345",
+        confirmPassword: "123",
+      });
+      clickOnSubmitButton();
+
+      expect(
+        screen.queryByText(/the passwords don't match. try again/i)
+      ).toBeInTheDocument();
+    });
+
+    test("should show no error message if every input is valid", () => {
+      typeIntoForm({
+        email: "selena@gmail.com",
+        password: "12345",
+        confirmPassword: "12345",
+      });
+      clickOnSubmitButton();
+
+      expect(
+        screen.queryByText(/the email you input is invalid/i)
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText(
+          /the password you entered should contain 5 or more characters/i
+        )
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText(/the passwords don't match. try again/i)
+      ).not.toBeInTheDocument();
+    });
+  });
 });
